c6466958924822ee10df2209138614be0c8ff706
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-22 00:00:19 +0530

    Modify: modify index.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Myprojects from './components/myprojects/Myprojects'
+import Contacts from './components/contacts/Contacts'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Myprojects/>
+        <Contacts/>
+        <Footer/>
+
+    </>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import './footer.css'
 
 const Footer = () => {
-  return import 
-  (
+  return (
     <div>Footer</div>
   )
 }
