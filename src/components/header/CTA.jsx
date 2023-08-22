38af95a8f78a8e62150aec077b136951fdf77212
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-22 23:55:24 +0530

    feat: create CTA.jsx file, add two buttons to "Header" component.

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CV from '../../assets/Thakshila-Bandara_CV.pdf'
+
+const CTA = () => {
+
+  return (
+    
+    <div className="cta">
+
+        <a href={CV} download className='btn'>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    
+    <header>
+
+      <div className="container header__container">
+
+        <h5>Hello, I'm</h5>
+        <h1>Thakshila Bandara</h1>
+        <h5 className="text-light">Software Engineering Undergraduate</h5>
+        <CTA/>
+      </div>
+    </header>
   )
 }
 
