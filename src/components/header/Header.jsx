5c78163f4a22dcd6b979d427f8fa92ea8ef7a0ad
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-23 13:33:12 +0530

    modify: add css to image in Header component.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import me from '../../assets/me.jpg'
+import me from '../../assets/me-new.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -16,7 +16,7 @@ const Header = () => {
         <h5 className="text-light">Software Engineering Undergraduate</h5>
         <CTA/>
         <HeaderSocials/>
-        
+
         <div className="me">
 
           <img src={me} alt='me'/>
