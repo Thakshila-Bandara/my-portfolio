358c8c4eee2548f79041643d6249d599ecd9790b
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-24 16:35:15 +0530

    feat: create about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 import './about.css'
+import myImage from '../../assets/my-image2.jpg'
+import {GiGraduateCap} from 'react-icons/gi'
+import {BsBookmarkStar} from 'react-icons/bs'
+import {TfiWorld} from 'react-icons/tfi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+
+          <div className="about__me">
+            <div className="about__me-image">
+                <img src={myImage} alt="About Image" />
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+
+              <article className='about__card'>
+                  <GiGraduateCap className='about__icon'/>
+                  <h5>Degree</h5>
+                  <small>Bsc.(Hons) Software Engneering(UG) <br /><i>University of Kelaniya.</i></small>
+              </article>
+
+              <article className='about__card'>
+                  <BsBookmarkStar className='about__icon'/>
+                  <h5>GPA</h5>
+                  <small>3.13</small>
+              </article>
+
+              <article className='about__card'>
+                  <TfiWorld className='about__icon'/>
+                  <h5>Domains</h5>
+                  <small>
+                    <ul>
+                      <li>Net Centric</li>
+                      <li>Mobile Applications</li>
+                    </ul>
+                  </small>
+              </article>
+
+              
+
+            </div>
+            <p>
+                    I'm a <b>Software Engineering Undergradute</b> at the University of Kelaniya. 
+                    I'm an enthusiastic and driven Software Engineering student seeking a challenging internship opportunity to apply 
+                    and expand my technical skills. 
+                    With a strong academic foundation in software engineering and hands-on experience in various programming languages,
+                    I am eager to contribute to innovative projects and learn from experienced professionals in the industry. I am a quick learner who is always ready to face challenges.
+                    
+            </p>
+
+          </div>
+      </div>
+    </section>
   )
 }
 
