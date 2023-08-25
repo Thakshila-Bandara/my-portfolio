4aafa65a13332014eda97a3fefce3cd0610ada49
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-25 23:25:47 +0530

    feat: create 'myprojects' section.

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './myprojects.css'
+import IMG1 from '../../assets/logo.png'
+import IMG2 from '../../assets/img2.png'
+import IMG3 from '../../assets/img3.png'
+import IMG4 from '../../assets/img4.png'
 
 const Myprojects = () => {
   return (
-    <section id='myprojects'>Myprojects</section>
+    <section id='myprojects'>
+
+      <h5>My Recent Works</h5>
+      <h2>Projects</h2>
+
+      <div className="container portfolio__container">
+
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG1} alt="" />
+            </div>
+            <h3>Fashion Gallery</h3>
+            <small className='text-light'>Spring Boot | ReactJs | MySQL</small><br /><br />
+            <a href="https://github.com/himani413/FashionGallery" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG2} alt="" />
+            </div>
+            <h3>Beauty Garden</h3>
+            <small className='text-light'>HTML | CSS | JS | php | MySQL</small><br /><br />
+            <a href="https://github.com/Thakshila-Bandara/Beauty-Garden" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG4} alt="" />
+            </div>
+            <h3>My Portfolio(This website)</h3>
+            <small className='text-light'>ReactJs</small><br /><br />
+            <a href="https://github.com/Thakshila-Bandara/my-portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG3} alt="" />
+            </div>
+            <h3>My Resume</h3>
+            <small className='text-light'>HTML | CSS | JS | Bootstrap</small><br /><br />
+            <a href="https://github.com/Thakshila-Bandara/my-resume" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            <a href="https://thakshila-bandara.github.io/my-resume/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+        </article>
+      </div>
+    </section>
   )
 }
 
