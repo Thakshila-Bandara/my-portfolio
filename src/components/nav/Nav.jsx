ffd2f2baf6dc6e5910c1609b3970a5d88e324cca
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-28 00:44:26 +0530

    fix: remove unwanted assets.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BiUser} from 'react-icons/bi'
-import {GiGraduateCap} from 'react-icons/gi'
+import {FaLaptopCode} from 'react-icons/fa'
 import {GoChecklist} from 'react-icons/go'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import { useState } from 'react'
@@ -19,7 +19,7 @@ const Nav = () => {
       
        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><BiUser/></a>
-       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><GiGraduateCap/></a>
+       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><FaLaptopCode/></a>
        <a href="#myprojects" onClick={() => setActiveNav('#myprojects')} className={activeNav === '#myprojects' ? 'active': ''}><GoChecklist/></a>
        <a href="#contacts" onClick={() => setActiveNav('#contacts')} className={activeNav === '#contacts' ? 'active': ''}><BiMessageSquareDetail/></a>
       
