8ad675fcee4e11949732a0e92bb17df85af80fcd
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-27 22:11:37 +0530

    feat: add email sending feature in "contacts" section using emailJs for React.

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './contacts.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contacts = () => {
+
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_kpzxgqz', 'template_kk9ipl8', form.current, 'uKELRhBOpArKOozDJ')
+    e.target.reset()
+    
+  }
+
   return (
     <section id='contacts'>
 
@@ -20,10 +33,10 @@ const Contacts = () => {
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
 
          <input type="text" name="name" placeholder="Your Full Name" required />  {/* client side validation */}
-         <input type="email" name='email' placeholder='Your Email' required />
+         <input type="email" name='email' placeholder='Your Email' required /> 
          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
          <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
