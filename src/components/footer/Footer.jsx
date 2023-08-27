3548315bff2c3d307837427c12660c37a052c520
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-28 00:08:31 +0530

    feat: create footer of the website

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsMedium} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <a href="#" className='footer__logo'>THAKSHILA</a>
+        <ul className="permalinks">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#myprojects">My Projects</a></li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="https://www.linkedin.com/in/thakshila-bandara-2791b9214"><BsLinkedin/></a>
+          <a href="https://github.com/Thakshila-Bandara"><BsGithub/></a>
+          <a href="https://medium.com/@thakshiladb2000"><BsMedium/></a>
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; Thakshila Bandara. All rightts reserved.</small>
+        </div>
+    </footer>
   )
 }
 
