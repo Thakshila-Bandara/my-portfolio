f06c456f872efe035ceec864b896f5d595964e3c
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-27 17:49:03 +0530

    modify: modify "myprojects" section.

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -19,33 +19,45 @@ const Myprojects = () => {
                 <img src={IMG1} alt="" />
             </div>
             <h3>Fashion Gallery</h3>
-            <small className='text-light'>Spring Boot | ReactJs | MySQL</small><br /><br />
-            <a href="https://github.com/himani413/FashionGallery" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            <small className='text-light'>Spring Boot | ReactJs | MySQL</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/himani413/FashionGallery" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            </div>
+              
         </article>
         <article className='portfolio__item'>
             <div className="portfolio__item-image">
                 <img src={IMG2} alt="" />
             </div>
             <h3>Beauty Garden</h3>
-            <small className='text-light'>HTML | CSS | JS | php | MySQL</small><br /><br />
-            <a href="https://github.com/Thakshila-Bandara/Beauty-Garden" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            <small className='text-light'>HTML | CSS | JS | php | MySQL</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/Thakshila-Bandara/Beauty-Garden" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            </div>
+            
         </article>
         <article className='portfolio__item'>
             <div className="portfolio__item-image">
                 <img src={IMG4} alt="" />
             </div>
             <h3>My Portfolio(This website)</h3>
-            <small className='text-light'>ReactJs</small><br /><br />
-            <a href="https://github.com/Thakshila-Bandara/my-portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            <small className='text-light'>ReactJs</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/Thakshila-Bandara/my-portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            </div>
+            
         </article>
         <article className='portfolio__item'>
             <div className="portfolio__item-image">
                 <img src={IMG3} alt="" />
             </div>
             <h3>My Resume</h3>
-            <small className='text-light'>HTML | CSS | JS | Bootstrap</small><br /><br />
-            <a href="https://github.com/Thakshila-Bandara/my-resume" target="_blank" rel='noreferrer' className='btn'>Github</a>
-            <a href="https://thakshila-bandara.github.io/my-resume/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+            <small className='text-light'>HTML | CSS | JS | Bootstrap</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/Thakshila-Bandara/my-resume" target="_blank" rel='noreferrer' className='btn'>Github</a>
+              <a href="https://thakshila-bandara.github.io/my-resume/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+            </div>
+            
         </article>
       </div>
     </section>
