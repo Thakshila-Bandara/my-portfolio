b8e8aa0c1354485f9870e37aaf218ec4ea1133c0
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-27 20:19:38 +0530

    feat: create "contacts" section and add media queries.

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './contacts.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contacts = () => {
   return (
-    <section id='contacts'>Contacts</section>
+    <section id='contacts'>
+
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>thakshiladb2000@gmail.com</h5>
+            <a href="mailto:thakshiladb2000@gmail.com">Send a message</a>
+          </article>
+        </div>
+
+        <form action="">
+
+         <input type="text" name="name" placeholder="Your Full Name" required />  {/* client side validation */}
+         <input type="email" name='email' placeholder='Your Email' required />
+         <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+         <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>    
+    </section>
   )
 }
 
